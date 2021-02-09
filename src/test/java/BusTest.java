8ff8b97e9d61ab9155d7f63d87513f36e527322f
ange47rm <ange47rm@hotmail.com>
2021-02-09 15:06:57 +0000

    Initial Lab Setup

---
+++ src/test/java/BusTest.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BusTest {
+
+    private Bus bus;
+    private Person person;
+
+    @Before
+    public void before(){
+        bus = new Bus("London", 10);
+        person = new Person();
+    }
+
+    @Test
+    public void busHasDestination(){
+        assertEquals("London", bus.getDestination());
+    }
+}
